a77587848e5d9838123e05313fb535bd98a8bed7
YuanSyun <rty3497374@gmail.com>
2017-06-20 20:12:46 +0800

    add button modify image src

---
+++ src/index.js
@@ -0,0 +1,17 @@
+function showApple(){
+	document.getElementById("showImage").src = "./Image/apple.png";
+}
+
+function btnR(){
+	document.getElementById("showImage").src = "./Image/apple.png";
+}
+
+function btnG(){
+	document.getElementById("showImage").src = "./Image/pear.png";
+	
+}
+
+function btnB(){
+	document.getElementById("showImage").src = "./Image/pineapple.png";
+}
+	
